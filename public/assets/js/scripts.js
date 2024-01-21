d67feba985b7a537022f2d87a9ec473d4ce5964a
DorkyP <64234510+DorkyP@users.noreply.github.com>
2024-01-21 21:28:15 +1100

    Update scripts.js to add links to socials

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -33,26 +33,26 @@ window.addEventListener('DOMContentLoaded', () => {
 				<br>
 				<ul class="list-inline text-center">
 					<li class="list-inline-item">
-						<a href="#!">
-							<span class="fa-stack fa-lg">
-								<i class="fas fa-circle fa-stack-2x"></i>
-								<i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
-							</span>
-						</a>
-					</li>
-					<li class="list-inline-item">
-						<a href="#!">
+						<a href="https://www.facebook.com/morningtonballetschool">
 							<span class="fa-stack fa-lg">
 								<i class="fas fa-circle fa-stack-2x"></i>
 								<i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
 							</span>
 						</a>
 					</li>
-					<li class="list-inline-item">
-						<a href="https://github.com/DorkyP/grumpyoldmanreviews">
+     					<li class="list-inline-item">
+						<a href="https://www.instagram.com/morningtonballetschool/">
 							<span class="fa-stack fa-lg">
 								<i class="fas fa-circle fa-stack-2x"></i>
-								<i class="fab fa-github fa-stack-1x fa-inverse"></i>
+								<i class="fab fa-instagram fa-stack-1x fa-inverse"></i>
+							</span>
+						</a>
+					</li>
+     					<li class="list-inline-item">
+						<a href="https://www.tiktok.com/@mornington.ballet">
+							<span class="fa-stack fa-lg">
+								<i class="fas fa-circle fa-stack-2x"></i>
+								<i class="fab fa-tiktok fa-stack-1x fa-inverse"></i>
 							</span>
 						</a>
 					</li>
